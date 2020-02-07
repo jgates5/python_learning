2fc8079a9bbf4754c46a858141880347bf76b41c
14357 <jeff.gates0507@gmail.com>
2020-02-07 16:10:10 -0700

    Add existing file

--- replace_functions_and string_values.py
+++ replace_functions_and string_values.py
@@ -1,2 +1,6 @@
-hello = ('jeff')
-print(hello)
+#replace function then reassign a set value
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+#sentence = 'New Value' #sentence variable is = to the above sentence but two diffent strings
+sentence = sentence.replace('quick', 'slow') #reassigned  a replacement word quick with slow
+print(sentence)
