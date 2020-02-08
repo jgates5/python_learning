5b4fd72a7467cbca8a1cf07427f483c7a90b8823
14357 <jeff.gates0507@gmail.com>
2020-02-08 14:39:07 -0700

    pythons strip, lstrip, and rstip functions

---
+++ strip_lstrip_and_rstrip.py
@@ -0,0 +1,12 @@
+#url = '     https://google.com      ' #strip spaces
+
+#print(url.strip)
+
+#url = 'https://google.com'
+
+#print(url.strip('https://'))
+
+#url = url.lstrip('https://')
+#url = url.rstrip('.com')
+
+#print(url.capitalize())
