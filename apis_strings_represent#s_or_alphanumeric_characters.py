f3f57a204abca39122cbca9f0e6051d9d994e8f6
14357 <jeff.gates0507@gmail.com>
2020-02-08 17:29:48 -0700

    How to Check if Strings Represent Numbers or Alphanumeric Characters in Python

---
+++ apis_strings_represent#s_or_alphanumeric_characters.py
@@ -0,0 +1,9 @@
+#api_data = '5'
+#greeting = 'Hi there'
+#greeting = 'Hithere' -> now is 100% alpanumeric
+#print(api_data.isalpha()) #is the 5 a set of alphanumeric characters or not? false
+#print(greeting.isalpha()) #the space between 'hi there' is not alplanumeric. the whole '' has to be 100% to be alpanumeric.  #is hi set of alphanumeric characters or not? true
+
+#use these 2 below often...
+#print(api_data.isnumeric()) #5 is a number
+#print(greeting.isnumeric() #)hi there is not a number
