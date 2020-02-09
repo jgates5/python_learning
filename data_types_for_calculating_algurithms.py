506da2e7dc518bb08c69c5b68028cb725ca3e025
14357 <jeff.gates0507@gmail.com>
2020-02-08 19:07:52 -0700

    order of operations

--- data_types_for_calculating_algurithms.py
+++ data_types_for_calculating_algurithms.py
@@ -7,25 +7,38 @@
 #print(product_id + new_product) #calculating two integers work to form an integer
 #print(tip_percentage)
 
-print('Addition')
-print(100 + 42)
+#print('Addition')
+#print(100 + 42)
 
-print('Subtraction')
-print(100 - 42)
+#print('Subtraction')
+#print(100 - 42)
 
-print('Division')
-print(100 / 42)
-print(100 / 38)
+#print('Division')
+#print(100 / 42)
+#print(100 / 38)
 
-print('Floor Division')
-print(100 // 42)
-print(100 // 38)
+#print('Floor Division')
+#print(100 // 42)
+#print(100 // 38)
 
-print('Multiplication')
-print(100 * 42)
+#print('Multiplication')
+#print(100 * 42)
 
-print('Modulus')
-print(100 % 42)
+#print('Modulus')
+#print(100 % 42) # checking to see if a number is an even or odd, any even number is goin to be 0 any odd number will return 1
 
-print('Exponents')
-print(100 ** 42)
+#print('Exponents')
+#print(100 ** 42)
+
+#order of operations
+#Please -> Parans ()
+#Excuse -> Exponents **
+#My -> Multiplication *
+#Dear -> Division /
+#Aunt -> Addition +
+#Sally -> Subtraction -
+"""
+calculation = 8 + 2 * 5 - (9 + 2) ** 2
+
+print(calculation)
+"""
