c97baef1bb85f1b4dbec51c2a2653d29a56cc919
14357 <jeff.gates0507@gmail.com>
2020-02-08 17:57:54 -0700

    mathematical operators in python

---
+++ data_types_for_calculating_algurithms.py
@@ -0,0 +1,31 @@
+#product_id = 123 #- > integer a whole number like 42, 32 and so forth
+#sale_price = 14.99 #-> this is a float type decimal number. A float percision is not the best choice for calculations
+#tip_percentage = 1/5
+#new_product = 150  #what happens when you combine integer data types
+#floats and sub-types can work together in calculations
+#print(sale_price + new_product) #integer and float can work together
+#print(product_id + new_product) #calculating two integers work to form an integer
+#print(tip_percentage)
+
+print('Addition')
+print(100 + 42)
+
+print('Subtraction')
+print(100 - 42)
+
+print('Division')
+print(100 / 42)
+print(100 / 38)
+
+print('Floor Division')
+print(100 // 42)
+print(100 // 38)
+
+print('Multiplication')
+print(100 * 42)
+
+print('Modulus')
+print(100 % 42)
+
+print('Exponents')
+print(100 ** 42)
