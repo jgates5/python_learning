2d9bf9b529935435c018ae31c4a87626e6c71f2a
14357 <jeff.gates0507@gmail.com>
2020-02-12 14:46:16 -0700

    building exponent function in python

---
+++ build_exponent_function.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+"""
+manual_exponent(2, 3)
+#> 8
+
+manual_exponent(10,2)
+#>100
+"""
+
+def manual_exponent(num, exp):
+    computed_list = [num] * exp
+    return(reduce(lambda total, element: total * element, computed_list))
+
+"""Computed_list = [3] * 2
+[3, 3]
+#counter = exp -1
+    total = num
+
+    while counter > 0:
+        total *= num
+        counter -= 1
+
+    return total
+"""
+print(manual_exponent(2, 3))
+print(manual_exponent(10,2))
+
+
