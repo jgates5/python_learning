2f77b396a7ff7cf1a6c1ac11bd394fb410ba3565
14357 <jeff.gates0507@gmail.com>
2020-02-13 15:36:39 -0700

    ranges for lists in python

---
+++ ranges_for_py_lists.py
@@ -0,0 +1,10 @@
+#list range
+tags = ['python', 'development', 'tutorials', 'code']
+tag_range = tags[1:3]# tutorials are not included
+#start from beginning to end of a lis range
+tag_range = tags[1:]
+tag_range = tags[:2]
+tag_range = tags[:-1] #starts to the back of the list and stops at the end code of -1
+tag_range = tags[:]
+print(tag_range)
+
