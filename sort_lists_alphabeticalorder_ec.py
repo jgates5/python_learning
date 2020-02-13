473b8f2692b5ddf134ae26d80d9774e6accdd440
14357 <jeff.gates0507@gmail.com>
2020-02-12 19:30:36 -0700

    organize lists in order or reverse with sort funcion

---
+++ sort_lists_alphabeticalorder_ec.py
@@ -0,0 +1,17 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+#sort shows the oldest value in the list
+tags.sort()
+
+print(tags)
+#if you would like the beginning of the newest in order first then use reverse
+tags.sort(reverse=True)
+
+print(tags)
+
+totals = [234, 1, 543, 2345]
+
+totals.sort()
+totals.sort(reverse=True)
+print(totals)
