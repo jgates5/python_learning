8fa5303cd0dbebc437a1974f5d46f853687003f1
14357 <jeff.gates0507@gmail.com>
2020-02-12 18:52:33 -0700

    working with queries, length, negative indexes in a python list

---
+++ listQueries_processes_length_negativeIndexes_index.py
@@ -0,0 +1,13 @@
+#length function gives you the full count in a number list
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+number_of_tags = len(tags)
+
+print(number_of_tags)
+
+#grabbing last item index in the list
+last_item = tags[-1]
+index_of_last_item = tags.index(last_item)
+print(last_item)
+print(index_of_last_item)
