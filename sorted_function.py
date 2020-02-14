5cb532de62e3b96e15c6c368ec85dac4bc8faa1c
14357 <jeff.gates0507@gmail.com>
2020-02-13 19:13:46 -0700

    using the sort function

---
+++ sorted_function.py
@@ -0,0 +1,19 @@
+
+#sorted list
+sale_prices = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+sale_prices.sort() #this value produces tegers that range from 1 to 499
+#method of sorted, allows to store a new value into a nother value
+sorted_list = sorted(sale_prices)
+sorted_list = sorted(sale_prices, reverse=True) top to bottom order
+print(sorted_list)
+print(sale_prices)
