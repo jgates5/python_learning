bd0ba6f9724ff12c6002287dc78702e3368cf0e9
14357 <jeff.gates0507@gmail.com>
2020-02-14 18:59:49 -0700

    working with slices to find median #

--- find_the_median_in_list_with_Odd_number of numbers.py
+++ find_the_median_in_list_with_Odd_number of numbers.py
@@ -19,7 +19,7 @@ sale_prices = [
   1,
   3
 ]
-
+"""
 sorted_list = sorted(sale_prices)
 num_of_sales = len(sorted_list)
 beginning_priced_items = sorted_list[:math.floor(num_of_sales/2)]
@@ -27,4 +27,12 @@ median = sorted_list[math.floor(num_of_sales/2):(math.floor(num_of_sales/2)+1)]
 print(sorted_list)
 print(num_of_sales)
 print(beginning_priced_items)
+print(median)
+"""
+list_prices = sorted(sale_prices)
+total_of_sale_items = len(list_prices)
+half_slice = math.floor(num_of_sales/2)
+median = list_prices[half_slice:(half_slice +1)]
+print(list_prices)
+print(total_of_sale_items)
 print(median)
