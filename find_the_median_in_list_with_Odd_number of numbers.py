e7a1319a8afa1e2bfcfeea83aff5aaa56d84b12d
14357 <jeff.gates0507@gmail.com>
2020-02-14 17:39:03 -0700

    learning exercise for how to find the mdian of a numbe ris slicing and math.floor library

---
+++ find_the_median_in_list_with_Odd_number of numbers.py
@@ -0,0 +1,30 @@
+import math
+
+"""
+Tools:
+- math library
+- sorted function
+- list slicing
+- computations
+"""
+
+sale_prices = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+
+sorted_list = sorted(sale_prices)
+num_of_sales = len(sorted_list)
+beginning_priced_items = sorted_list[:math.floor(num_of_sales/2)]
+median = sorted_list[math.floor(num_of_sales/2):(math.floor(num_of_sales/2)+1)]
+print(sorted_list)
+print(num_of_sales)
+print(beginning_priced_items)
+print(median)
--- popular_math_functions.py
+++ popular_math_functions.py
@@ -5,7 +5,7 @@ import math
 
 loss = -20.25
 product_cost = 89.99
-
+"""
 print(abs(loss))
 print(math.floor(product_cost)) -> shows the lowest number to product cost
 print(math.ceil(product_cost)) -> shows the rounded up value of a number
@@ -13,4 +13,5 @@ print(abs(math.floor(loss)) -> nesting functions
 print(round(product_cost)) -> round to the nearest whole number
 print(math.sqrt(product_cost))floating point value
 print(math.pow(5, 2)) -> takes two arguments
-print(5 ** 2)
+print(5 ** 2)"""
+print(math.floor)
