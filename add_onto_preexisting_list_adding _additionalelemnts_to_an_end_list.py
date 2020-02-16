4574426aa768c9ce49977d771f8ed0c41f65273b
14357 <jeff.gates0507@gmail.com>
2020-02-16 14:51:54 -0700

    adding elements to a preexisitng list, and adding elements to the end of that list

---
+++ add_onto_preexisting_list_adding _additionalelemnts_to_an_end_list.py
@@ -0,0 +1,17 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+#Not the right way
+#replacing code with programming 
+#[tags-1] = 'programming'
+
+#spreads the word to a programming to their own haracters is nope
+#tags.extend('porgramming')
+#print(tags) #==['python', 'development', 'tutorials', 'code', 'p', 'o', 'r', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+
+#to use the value extend you need to wrap the word programming within its own element[]
+#tags.extend(['programming'])
+#print(tags)
+
+#(creating a new list by adding the new element at the end of that list)
+new_tags = tags + ['Programming']
+print(new_tags)
