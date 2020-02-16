ca0b55b13a21cc70f7701a45623883a00c55ef53
14357 <jeff.gates0507@gmail.com>
2020-02-16 16:48:15 -0700

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+#dictionary is what's called a key-value data store
+#dictionary that is stored the players variable has a key of SS and a value of Correa
+players = {
+  "ss": "Correa",
+  "2b": "Altuve",
+  "3b": "Bregman",
+  "DH": "Gattis",
+  "OF": "Springer",
+}
+
+second_base = players['2b']
+designated_hitter = players['DH']
+
+print(designated_hitter)
