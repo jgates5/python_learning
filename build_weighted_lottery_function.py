203fc5ded09e444ae8976f5c912c9a94bd17420b
14357 <jeff.gates0507@gmail.com>
2020-02-16 15:26:50 -0700

    building weighted lottery with numpy library

---
+++ build_weighted_lottery_function.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def weighted_lottery(weights):
+    container_list = []
+
+    for (name, weight) in weights.items():
+        # loop 1 time
+        #loop 1000 times
+        for _ in range(weight):
+            container_list.append(name)
+    return np.random.choice(container_list)
+
+
+other_weights = {
+    'winning': 1,
+    'break_even': 2,
+    'losing': 3
+    }
+
+print(weighted_lottery(other_weights))
+
+
