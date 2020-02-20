1bf5ed8b45665713484e11ffe10b280579d63399
14357 <jeff.gates0507@gmail.com>
2020-02-19 17:54:35 -0700

    build dictionary of a histogram

--- add_newKey/valuepairs_to dictionaries.py
+++
@@ -1,9 +0,0 @@
-teams = {
-  "astros": ["Altuve", "Correa", "Bregman"],
-  "angels": ["Trout", "Pujols"],
-  "yankees": ["Judge", "Stanton"],
-}
-#add a new key to an existing list inside a dictionary
-teams['red sox'] = ['Price', 'Betts']
-
-print(teams)
