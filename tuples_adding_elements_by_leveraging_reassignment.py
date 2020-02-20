a79b2010fcfe39615a18aa09e7675fd5c66968a7
14357 <jeff.gates0507@gmail.com>
2020-02-19 19:09:35 -0700

    use reassignment of an element and add the tuple in the list

---
+++ tuples_adding_elements_by_leveraging_reassignment.py
@@ -0,0 +1,44 @@
+#Leveraging reassignment
+# adding a Tuple in parenthesis
+#post = post + ('published',) 
+#need to add a [,] to make it a tuple
+# can only concatenate tuple (not "str") to tuple
+
+
+post = ('Python Basics', 'Intro guide to python', 'Some cool python content')
+"""
+create two objects add them together so we're concatenating them and then 
+put them inside of this variable and it's simply going to override it.
+"""
+
+#post = post + ('published',) 
+post += ('published',) 
+
+title, sub_heading, content, status = post
+
+print(title)
+print(sub_heading)
+print(content)
+print(status)
+
+"""Python Basics
+Intro guide to python
+Some cool python content
+published"""
+
+"""post = ('Python Basics', 'Intro guide to python', 'Some cool python content')
+
+print(id(post))
+print(id(post))
+
+post += ('published',)
+
+print(id(post))
+
+title, sub_heading, content, status = post
+
+print(title)
+print(sub_heading)
+print(content)
+print(status)
+"""
