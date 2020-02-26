4120dad6e986fe591f0685d52b637f4cf59aef1e
14357 <jeff.gates0507@gmail.com>
2020-02-26 16:12:13 -0700

    SET dat structures in python

---
+++ SET_data_structure.py
@@ -0,0 +1,34 @@
+tags = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+#print(tags) #{'coding', 'tutorials', 'python'}
+
+#a set similar like a list but cnt use duplicates a set be good
+
+#print(tags[0])
+
+query_one = 'python' in tags #true
+query_two = 'ruby' in tags #false
+
+print(query_one)
+tags = {
+  'python',
+  'coding',
+  'tutorials',
+  'coding'
+}
+
+#print(tags) #{'coding', 'tutorials', 'python'}
+
+#a set similar like a list but cnt use duplicates a set be good
+
+#print(tags[0])
+
+query_one = 'python' in tags #true
+query_two = 'ruby' in tags #false
+
+print(query_two)
