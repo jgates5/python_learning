15e347798a4df6e814069a083f1682bf75c4c77e
14357 <jeff.gates0507@gmail.com>
2020-02-26 15:11:33 -0700

    updated id post

--- tuples_adding_elements_by_leveraging_reassignment.py
+++ tuples_adding_elements_by_leveraging_reassignment.py
@@ -6,21 +6,23 @@
 
 
 post = ('Python Basics', 'Intro guide to python', 'Some cool python content')
-"""
+
+print(id(post))
+print(id(post))
+#post = post + ('published',) 
+post += ('published',) #-> """
 create two objects add them together so we're concatenating them and then 
 put them inside of this variable and it's simply going to override it.
 """
+print(id(post))
+#title, sub_heading, content, status = post
 
-#post = post + ('published',) 
-post += ('published',) 
-
-title, sub_heading, content, status = post
-
+"""
 print(title)
 print(sub_heading)
 print(content)
 print(status)
-
+"""
 """Python Basics
 Intro guide to python
 Some cool python content
