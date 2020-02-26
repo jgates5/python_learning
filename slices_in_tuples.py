3f51cbd1d039cfce94b3cdb48eab8c96d5aa4c42
14357 <jeff.gates0507@gmail.com>
2020-02-26 15:37:01 -0700

    removing tuples

--- slices_in_tuples.py
+++ slices_in_tuples.py
@@ -1,3 +1,3 @@
 post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
 #print(post[:2])('Python Basics', 'Intro guide to Python')
-print(post[1::2]) 
+print(post[1::2]) ('Intro guide to Python', 'published')
