7e057c85d48ef16f18d19031b98b1326258d0322
14357 <jeff.gates0507@gmail.com>
2020-02-26 18:10:05 -0700

    Using string interpellation for a heading tag

---
+++ build_html_heading_generating function.py
@@ -0,0 +1,17 @@
+"""
+heading_generator(title, heading_type)
+heading_generator('Greeting', '1')
+<h1>Greeting</h1>
+heading_generator('Hi there', '3')
+<3>Hi there</h3>
+"""
+
+#working with string interpolation
+#have some kind of heading and title argument and heading type
+
+def heading_generator(title, heading_type):
+    return f' <h{heading_type}>{title}</h{heading_type}>'
+
+print(heading_generator('Greeting', '1'))
+
+print(heading_generator('Hi there', '3'))
