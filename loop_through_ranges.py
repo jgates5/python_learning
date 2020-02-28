697951ca9c5b87219584f0a6656e3a9156a3c59e
14357 <jeff.gates0507@gmail.com>
2020-02-27 17:50:05 -0700

    rangees in looping

---
+++ loop_through_ranges.py
@@ -0,0 +1,15 @@
+#range takes 2 arguments range goes up to 9 1 by off number value
+"""for num in range(1, 10):
+    print(num)"""
+
+#skipping values
+
+for num in range(1, 11, 2):
+    print(num) 
+"""
+1
+3
+5
+7
+9
+"""
