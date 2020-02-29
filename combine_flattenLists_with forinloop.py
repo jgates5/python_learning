581402a230d29edc86d6c2e7bbfe6c227a7d43e6
14357 <jeff.gates0507@gmail.com>
2020-02-28 19:06:06 -0700

    working with for in loops in python

---
+++ combine_flattenLists_with forinloop.py
@@ -0,0 +1,12 @@
+legacy_customers = ['Alice', 'Bob']
+new_customers = ['Tiffany', 'Kristine']
+
+raw_db = [legacy_customers, new_customers]
+print(raw_db)
+#[['Alice', 'Bob', 'Tiffany', 'Kristine']]
+#[['Alice', 'Bob', Tiffany', 'Kristine']]
+
+for legacy_customer in legacy_customers:
+    new_customers.append(legacy_customer)
+
+print(new_customers)
