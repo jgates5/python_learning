deda3b85e4a3ee7173f785bc95c1dfa13bc74ba8
14357 <jeff.gates0507@gmail.com>
2020-03-03 16:55:17 -0700

    different comparisons of working with operators in conditionals

---
+++ comparison_operator.py
@@ -0,0 +1,32 @@
+# a conditional looks for a true or false value
+# List of comparison operators
+# == Equality
+# != Inequality
+# <> Inequality (deprecated)
+# >  Greater than
+# >= Greater than or equal to
+# < Less than
+# <= Less than or equal to
+"""
+username = 'johnsnow'
+
+if username == 'johnsnow':
+    print('Welcome')
+else:
+    print('You shall not pass')
+"""
+"""
+username = 'johnsnow'
+
+if username != 'johnsnow':
+    print('Welcome')
+else:
+    print('You shall not pass')
+    """
+"""
+user_list = ['Kristine', 'Tiffany']
+second_list =  ['Kristine', 'Tiffany']
+
+if user_list == second_list:
+    print('They match')
+"""
