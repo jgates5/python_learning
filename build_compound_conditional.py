046f8ac4d0bb672bac502fb3619f88af4e621d2d
14357 <jeff.gates0507@gmail.com>
2020-03-04 14:15:58 -0700

    adding the and not And function with conditionals

---
+++ build_compound_conditional.py
@@ -0,0 +1,44 @@
+username = 'jonsnow'
+email = 'jon@snow.com'
+password = 'thenorth'
+
+
+#using the and function
+"""
+if username == 'jonsow' and password == 'thenorth':
+    print('Access permitted')
+else:
+    print('You shall not pass')
+#You shall not pass
+"""
+ #if i did not want thr and function i can swap out with the OR function
+
+#the OR function looks to the left expression if its true it skis the entire right side
+if username == 'jonsow' or password == 'thenorth':
+    print('Access permitted')
+else:
+    print('You shall not pass')
+#Access Permitted
+#Access Permitted
+
+#the third condition of a real world vision?
+"""
+if(username)
+
+if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+  print('Access permitted')
+else:
+  print('Not allowed')
+"""
+#we want to see if guest user or admin user
+logged_in = True
+standard_user = True
+#to see if one side is true and the other side is not
+#because and not the right hand side has to be false.
+# you say and both sides has to be true in order for the entire expression to be true
+if logged_in and not standard_user: #if they are logged in  and not standard_user has to be false44
+
+    print('You can access the admin dashboard')
+else:
+    print('You can only access the standard dashboard')
+#Access permitted, You can only access the standard dashboard
