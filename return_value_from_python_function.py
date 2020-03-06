1c34954c139658417ba950736d63551ff795eda5
14357 <jeff.gates0507@gmail.com>
2020-03-06 15:18:21 -0700

    print and return statements and calling it wherever in the program

---
+++ return_value_from_python_function.py
@@ -0,0 +1,18 @@
+"""
+def full_name(first, last):
+    print(f'{first} {last}')
+
+full_name('Kristine', 'Hudgens')
+"""
+#understanding output and input process
+#input is the aruguements first and last name, output is nothingneed a return statement ouput.
+
+def full_name(first, last):
+    return f'{first} {last}'
+
+Kristine = full_name('Kristine', 'Hudgens')
+
+def greeting(name):
+    print(f'Hi {name}!')
+
+greeting(Kristine)
