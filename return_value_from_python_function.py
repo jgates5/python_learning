319eee287d2c1f809a4da1023e12f5da05f441dc
14357 <jeff.gates0507@gmail.com>
2020-03-06 15:19:27 -0700

    Nest Functions in Parent Functions in Python

--- return_value_from_python_function.py
+++ return_value_from_python_function.py
@@ -1,7 +1,7 @@
 """
 def full_name(first, last):
     print(f'{first} {last}')
-
+#print function onlys allows you to see what the syntax error is if your trying to locate something.
 full_name('Kristine', 'Hudgens')
 """
 #understanding output and input process
@@ -9,10 +9,11 @@ full_name('Kristine', 'Hudgens')
 
 def full_name(first, last):
     return f'{first} {last}'
-
+#return values show the results in te real world
 Kristine = full_name('Kristine', 'Hudgens')
 
 def greeting(name):
     print(f'Hi {name}!')
 
 greeting(Kristine)
+#Hi Kristine Hudgens!
