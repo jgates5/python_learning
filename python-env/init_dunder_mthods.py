0580ce2aeb2c19f725606b9490ba118ba87946db
14357 <jeff.gates0507@gmail.com>
2020-03-20 15:36:42 -0600

    init dunder repr collecting raw data

--- python-env/init_dunder_mthods.py
+++ python-env/init_dunder_mthods.py
@@ -3,11 +3,18 @@ class Invoice:
       self.client = client
       self.total = total
 
+#raw data inside of a class
   def __str__(self):
-    return f'Invoice from {self.client} for  {self.total}'
+    return f'Invoice from {self.client} for {self.total}'
+
+  def __rep__(self):
+    return f'Invoice <value: client: {self.client} total: {self.total}>'
 
 inv = Invoice('Google', 500)
 print(str(inv))
+print(repr(inv))
+
+#the goals for dunder reper more for raw output to logs
 
 #double underscores in front of the name. The reason for this speaks directly to how python works with private and protected methods inside of their classes
 # __init__
