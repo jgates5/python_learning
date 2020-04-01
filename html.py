445b67961c9d71b4428b4a7cde2a9cd93973b8a0
14357 <jeff.gates0507@gmail.com>
2020-04-01 16:02:52 -0600

    building functions in polymorfism

--- html.py
+++ html.py
@@ -18,26 +18,27 @@
 #         ]
 # for tag in tags:
 #     print(tag.render())
-
-class Html:
+class Heading:
     def __init__(self, content):
-        self.content = content
+      self.content = content
 
-    def render(self):             
-        raise NotImplementedError("Subclass must implement render method")
-
-
-class Heading(Html):
     def render(self):
-        return f'<h1>{self.content}</h1>'
+      return f'<h1>{self.content}</h1>'
 
+class Div:
+  def __init__(self, content):
+    self.content = content
 
-class Div(Html):
-    def render(self):
-        return f'<div>{self.content}</div>'
+  def render(self):
+    return f'<div>{self.content}</div>'
 
+div_one = Div('Some content')
+heading = Heading('My Amazing Heading')
+div_two = Div('Another div')
 
-tags = [Div('Some content'), Heading('My Amazing Heading'), Div('Another div')]
+def html_render(tag_object):
+  print(tag_object.render())
 
-for tag in tags:
-    print(tag.render())
+html_render(div_one)
+html_render(div_two)
+html_render(heading)
