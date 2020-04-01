cfbd96bf539f96ce1470311c017f749bbb8ce0fd
14357 <jeff.gates0507@gmail.com>
2020-04-01 15:14:16 -0600

    Polymorfism

---
+++ html.py
@@ -0,0 +1,43 @@
+# class Html:
+#     def __init(self, content):
+#         self.content = content
+#     def render(self):
+#         raise NotImplementedError('Subclass must implement render method')
+
+# class Heading(Html):
+#     def render(self):
+#         return f'<h1>{self.content}</h1>'
+
+# class Div(Html):
+#     def render(self):
+#         return f'<div>{self.content}</div>'
+
+# tags = [
+#         Heading('Some big heading'),
+#         Div('Another div')
+#         ]
+# for tag in tags:
+#     print(tag.render())
+
+class Html:
+    def __init__(self, content):
+        self.content = content
+
+    def render(self):             
+        raise NotImplementedError("Subclass must implement render method")
+
+
+class Heading(Html):
+    def render(self):
+        return f'<h1>{self.content}</h1>'
+
+
+class Div(Html):
+    def render(self):
+        return f'<div>{self.content}</div>'
+
+
+tags = [Div('Some content'), Heading('My Amazing Heading'), Div('Another div')]
+
+for tag in tags:
+    print(tag.render())
